5ef4b42c41a475b42e7190354b63cd3b51360111
Stanislav Potemkin <woodger@ya.ru>
2021-02-16 16:15:51 +0300

    Move the Node in abstract class

--- test/node.spec.ts
+++ test/node.spec.ts
@@ -5,14 +5,4 @@ describe('class nebbia.Node', () => {
   it('The "class Node" must be of function type', () => {
     assert(typeof nebbia.Node === 'function');
   });
-
-  it('constructor: new Node()', () => {
-    const node = new nebbia.Node();
-
-    assert(node.type === null);
-    assert(node.name === null);
-    assert(node.parent === null);
-    assert(node.value === '');
-    assert(node.childs instanceof Array);
-  });
 });
