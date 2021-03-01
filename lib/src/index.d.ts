db6df9794e27dedb041c73e069b32e0f829e27ef
Stanislav Potemkin <woodger@ya.ru>
2021-03-01 15:14:20 +0300

    Improve named the Hybrid interface function

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -3,11 +3,11 @@ import Expression from './expression';
 import Statement from './statement';
 import Text from './text';
 import parse from './parse';
-interface Nebbia {
+export interface INebbia {
     (template: string): string;
     [key: string]: any;
 }
-declare const _default: Nebbia & {
+declare const _default: INebbia & {
     Node: typeof Node;
     Expression: typeof Expression;
     Statement: typeof Statement;
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import Statement from './statement';
 import Text from './text';
 import parse from './parse';
 
-interface Nebbia {
+export interface INebbia {
   (template: string): string;
   [key: string]: any
 }
 
-const nebbia = <Nebbia>function(template: string): string {
+const nebbia = <INebbia>function(template: string): string {
   return parse(template).build();
 }
 
